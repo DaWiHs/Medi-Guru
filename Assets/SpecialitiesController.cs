fdebc954f26fdd0f33a517b4a5166126fab8790c
DaWiHs <29844077+DaWiHs@users.noreply.github.com>
2023-05-04 01:05:32 +0200

    Specialities render + prefab Spec. button delegate template

--- Assets/SpecialitiesController.cs
+++ Assets/SpecialitiesController.cs
@@ -1,34 +1,104 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SpecialitiesController : MonoBehaviour
 {
+    [Header("References")]
+    [SerializeField] GameObject specialityPrefab;
+    [SerializeField] RectTransform specialitiesParent;
 
-    [SerializeField] WebResponse myResponse = new WebResponse();
+    [Header("Variables")]
+    public List<string> specialities = new List<string>();
+    public Color toggleDisabled;
+    public Color toggleEnabled;
 
-    // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(GetSpecialities());
+        RenderSpecialities();
     }
 
     // Update is called once per frame
     void Update()
     {
+
     }
 
-    IEnumerator GetSpecialities()
+    public void RenderSpecialities() { StartCoroutine(_RenderSpecialities()); }
+    private IEnumerator _RenderSpecialities()
     {
-        
+        // Get specialities if none
+        if (specialities.Count < 1) yield return StartCoroutine(GetSpecialities());
+
+        // Render
+        int currentY = 0;
+        bool isLeft = true;
+        foreach (string speciality in specialities)
+        {
+            // Generate
+            GameObject obj = Instantiate(specialityPrefab, specialitiesParent);
+            RectTransform t = obj.GetComponent<RectTransform>();
+
+            // Place
+            t.anchorMin = new Vector2(isLeft ? 0f : 0.5f, 1);
+            t.anchorMax = new Vector2(isLeft ? 0.5f : 1f, 1);
+
+            t.anchoredPosition = new Vector2Int(0, currentY);
+
+            // Obj update
+            obj.GetComponentInChildren<Text>().text = speciality;
+            string _s = speciality;
+            Button _b = obj.GetComponentInChildren<Button>();
+            _b.onClick.AddListener(delegate { ToggleSpeciality(_s, _b); });
+
+
+
+            // Render options update
+            if (isLeft)
+            {
+                isLeft = false;
+            } else
+            {
+                isLeft = true;
+                currentY -= 20;
+            }
+            
+        }
+
+    }
+
+    public IEnumerator GetSpecialities()
+    {
+        WebResponse response = new WebResponse();
+
         yield return StartCoroutine(WebRequest.instance.Request(
             "GET", 
             "https://raw.githubusercontent.com/hykare/mediguru/devise-api/lib/specialties.txt",
             "",
-            myResponse));
+            response));
 
-        Debug.Log(myResponse);
-        Debug.Log(myResponse.content);
+        specialities.AddRange(response.content.Split('\n'));
+        specialities.Sort();
+
+    }
+
+    public void ToggleSpeciality(string speciality, Button reference)
+    {
+        // Profile update
+        // Toggle image update
+        Image _img = reference.transform.GetChild(0).GetComponentInChildren<Image>();
+        
+        if (_img.color == toggleEnabled)
+        {
+            _img.color = toggleDisabled;
+            // Profil update TODO
+        } else
+        {
+            _img.color = toggleEnabled;
+            // Profil Update TODO 
+        }
+
     }
 
 }
