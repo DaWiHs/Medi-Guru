7be0e9dbe8dc29717dcb6d85e46c1bc52b3ded41
DaWiHs <29844077+DaWiHs@users.noreply.github.com>
2023-05-05 13:47:29 +0200

    Dropdown search Search Re-render
    MyInputField : InputField:
    - OnFocus(delegate <>)
    - OnLostFocus(delegate <>)

--- Assets/SpecialitiesController.cs
+++ Assets/SpecialitiesController.cs
@@ -8,6 +8,10 @@ public class SpecialitiesController : MonoBehaviour
     [Header("References")]
     [SerializeField] GameObject specialityPrefab;
     [SerializeField] RectTransform specialitiesParent;
+    [SerializeField] InputField searchInput;
+    [SerializeField] MyInputField mySearchInput;
+
+    [SerializeField] Dictionary<string, GameObject> searchValues = new Dictionary<string, GameObject>();
 
     [Header("Variables")]
     public List<string> specialities = new List<string>();
@@ -17,12 +21,21 @@ public class SpecialitiesController : MonoBehaviour
     void Start()
     {
         RenderSpecialities();
+        searchInput.onValueChanged.AddListener(delegate { SearchSpecialities(); });
+
+        mySearchInput.OnFocus(delegate { CallTest("OnFocus MyInputField"); });
+        mySearchInput.OnLostFocus(delegate { CallTest("OnLostFocus MyInputField"); });
     }
 
     // Update is called once per frame
     void Update()
     {
 
+    }
+
+    public void CallTest(string text)
+    {
+        Debug.Log(text);
     }
 
     public void RenderSpecialities() { StartCoroutine(_RenderSpecialities()); }
@@ -33,36 +46,31 @@ public class SpecialitiesController : MonoBehaviour
 
         // Render
         int currentY = 0;
-        bool isLeft = true;
         foreach (string speciality in specialities)
         {
             // Generate
             GameObject obj = Instantiate(specialityPrefab, specialitiesParent);
             RectTransform t = obj.GetComponent<RectTransform>();
 
-            // Place
-            t.anchorMin = new Vector2(isLeft ? 0f : 0.5f, 1);
-            t.anchorMax = new Vector2(isLeft ? 0.5f : 1f, 1);
+            searchValues.Add(speciality, obj);
 
+            // Place
             t.anchoredPosition = new Vector2Int(0, currentY);
 
             // Obj update
             obj.GetComponentInChildren<Text>().text = speciality;
             string _s = speciality;
             Button _b = obj.GetComponentInChildren<Button>();
-            _b.onClick.AddListener(delegate { ToggleSpeciality(_s, _b); });
 
+            _b.onClick.AddListener(delegate { SelectSpeciality(_s, _b); });
+
+            obj.name = "Speciality_" + speciality;
+
+            
 
 
             // Render options update
-            if (isLeft)
-            {
-                isLeft = false;
-            } else
-            {
-                isLeft = true;
-                currentY -= 20;
-            }
+            currentY -= 20 + 1;
             
         }
 
@@ -83,7 +91,7 @@ public class SpecialitiesController : MonoBehaviour
 
     }
 
-    public void ToggleSpeciality(string speciality, Button reference)
+    public void SelectSpeciality(string speciality, Button reference)
     {
         // Profile update
         // Toggle image update
@@ -98,7 +106,36 @@ public class SpecialitiesController : MonoBehaviour
             _img.color = toggleEnabled;
             // Profil Update TODO 
         }
+    }
 
+    private void SearchSpecialities()
+    {
+        List<string> found = new List<string>();
+        string searchWord = searchInput.text;
+
+        foreach (string item in specialities)
+        {
+            if (item.Contains(searchWord)) found.Add(item);
+        }
+
+        int currentY = 0;
+        foreach (string speciality in specialities)
+        {
+            if (found.Contains(speciality))
+            {
+                // Place
+                searchValues[speciality].SetActive(true);
+                searchValues[speciality].GetComponent<RectTransform>().anchoredPosition = new Vector2Int(0, currentY);
+
+                // Render options update
+                currentY -= 20 + 1;
+            }
+            else
+            {
+                // Hide button
+                searchValues[speciality].SetActive(false);
+            }
+        }
     }
 
 }
