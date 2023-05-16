32c40c26c030ca0811fe19c7bc6d2f2f463a24f1
DaWiHs <29844077+DaWiHs@users.noreply.github.com>
2023-05-16 14:15:55 +0200

    DebugMenu

--- Assets/Scripts/DebugScript.cs
+++ Assets/Scripts/DebugScript.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DebugScript : MonoBehaviour
 {
@@ -8,11 +9,13 @@ public class DebugScript : MonoBehaviour
     public GameObject appView;
     public GameObject loginView;
 
+    public GameObject debugMenu;
+    public InputField urlInput;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        urlInput.text = WebRequest.instance.url;
     }
 
     // Update is called once per frame
@@ -31,6 +34,22 @@ public class DebugScript : MonoBehaviour
     {
         appView.SetActive(false);
         loginView.SetActive(true);
+    }
+    public void OpenDebugMenu()
+    {
+        debugMenu.SetActive(true);
+    }
+    public void CloseDebugMenu()
+    {
+        debugMenu.SetActive(false);
+    }
+    public void ApplyURL()
+    {
+        WebRequest.instance.url = urlInput.text;
+    }
+    public void TestConnectivity()
+    {
+
     }
 
 }
